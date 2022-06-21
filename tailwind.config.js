0931f0b6b1132f99f6e06d050a4194baca41a285
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-06-21 23:48:23 +0700

    feat: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetBrainsMono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        gray: {
+          50: '#ffffff',
+          100: '#d6d6d6',
+          200: '#adadad',
+          300: '#848484',
+          400: '#5b5b5b',
+          500: '#323232',
+          600: '#282828',
+          700: '#1e1e1e',
+          800: '#141414',
+          900: '#0a0a0a'
+        },
+      }
+    },
+  },
+  plugins: [],
+};

a1d259e431eca83b38791d319170788bc7aa0980
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-06-21 21:27:08 +0700

    refactor: bootstrap

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports es2021
+    target: ['es2021', 'chrome97', 'safari13'],
+    // don't minify for debug builds
+    minify: (!process.env.TAURI_DEBUG) as boolean && 'esbuild',
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  plugins: [react()]
+});

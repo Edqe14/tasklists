306ef1f6947867c942d18b056ffd69069597b2d5
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-06-22 00:04:55 +0700

    refactor: apply dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Set dark theme if applicable
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(

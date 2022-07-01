b485c51f0eb8c7240a3aa1804ce6ef406272f87e
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-07-02 02:06:33 +0700

    chore: fuck test

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,10 @@
-import { BrowserReporter, Consola, LogLevel } from 'consola';
+import { BrowserReporter, Consola, ConsolaReporter, LogLevel } from 'consola';
 
 const logger = new Consola({
   level: import.meta.env.DEV ? LogLevel.Verbose : LogLevel.Info,
   reporters: [
     new BrowserReporter()
-  ],
+  ].filter(Boolean) as ConsolaReporter[],
 });
 
 export default logger;

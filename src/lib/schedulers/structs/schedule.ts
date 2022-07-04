11bbec738d82831676e6658370c6d9a0e18efd38
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-07-04 19:36:24 +0700

    feat: add basic scheduler struct, untested

---
+++ src/lib/schedulers/structs/schedule.ts
@@ -0,0 +1,84 @@
+import { EventEmitter } from 'eventemitter3';
+import { nanoid } from 'nanoid';
+import { Mixin } from 'ts-mixer';
+import TypedEventEmitter from 'typed-emitter';
+import Timestamps from '@/lib/store/structs/timestamps';
+
+export type ScheduleEvents = {
+  // eslint-disable-next-line no-use-before-define
+  run: (value: Schedule) => void;
+};
+
+export interface ScheduleOptions<T = unknown> {
+  time: number | Date;
+  reference?: T;
+  reoccuring?: boolean;
+  relative?: boolean;
+}
+
+class Schedule<T = unknown> extends Mixin(Timestamps, EventEmitter as new () => TypedEventEmitter<ScheduleEvents>) {
+  protected _enabled = true;
+
+  get enabled() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._enabled;
+  }
+
+  public readonly id = nanoid();
+
+  public reference?: T;
+
+  public reoccuring: boolean;
+
+  public time: number;
+
+  public relative: boolean;
+
+  private timeout: NodeJS.Timeout | null = null;
+
+  constructor({ reference, reoccuring, time, relative }: ScheduleOptions<T>) {
+    if ((reoccuring || relative) && time instanceof Date) throw new TypeError('Reoccuring or relative schedules must be given a time in milliseconds');
+
+    super();
+
+    this.reference = reference;
+    this.reoccuring = reoccuring ?? false;
+    this.relative = relative ?? false;
+    this.time = time instanceof Date ? time.getTime() : time;
+
+    this.schedule();
+  }
+
+  public setEnabled(value: boolean) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._enabled = value;
+
+    if (!value && this.timeout) {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+
+      return;
+    }
+
+    if (value && !this.timeout) return this.schedule();
+  }
+
+  public reschedule(time: ScheduleOptions['time']) {
+    if ((this.reoccuring || this.relative) && time instanceof Date) throw new TypeError('Reoccuring or relative schedules must be given a time in milliseconds');
+
+    this.time = time instanceof Date ? time.getTime() : time;
+    this.schedule();
+  }
+
+  protected schedule() {
+    if (this.timeout) clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.emit('run', this);
+
+      if (this.reoccuring) this.schedule();
+    }, this.relative ? this.time : this.time - Date.now());
+  }
+}
+
+export default Schedule;
